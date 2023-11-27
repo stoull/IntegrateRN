f38f2d6245f87b62c516a25aa2e932ad3d8270fc
stoull <stoull@users.noreply.github.com>
2023-11-27 23:35:35 +0800

    Initial commit

---
+++ iOS/IntegrateRNiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntegrateRNiOS
+//
+//  Created by Stoull Hut on 2023/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Home"
+    }
+
+
+}
+
